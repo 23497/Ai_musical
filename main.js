9b39c6bb44dadbd1ec6260f6e04fb135ba38424a
23497 <79152493+23497@users.noreply.github.com>
2021-08-05 15:37:37 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 song = "";
 song2 = "";
+play_song = song.play();
+play_song2 = song2.play();
 
 function preload(){
     song = loadSound("music.mp3");
@@ -20,4 +22,43 @@ function setup(){
 function modelLoaded() 
 {
     console.log('poseNet is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    fill('#FF0000');
+    stroke('#FF0000');
+    
+
+    if(scoreLeftWrist > 0.2)
+    {
+    circle(rightWristX,rightWristY,20);
+
+    song2.stop()
+
+    if(rightWristY > 0 && rightWristY <= 500)
+    {
+        
+    }
+}
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist =" + scoreLeftWrist + "scoreRightWrist =" + scoreRightWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = "+ rightWristY);
+    }
 }
